4d8fdda278d1daa4d886fb56f2f069a15629d70d
n-is <nschl.npl@gmail.com>
2020-05-16 20:22:40 +0545

    Decouple processor and processor pool.

--- core/pipeline/processor.go
+++ core/pipeline/processor.go
@@ -6,45 +6,15 @@ import (
 	"sync/atomic"
 )
 
-// IProcessorExecutor defines the interface for a processor from the perspective of an Executor.
-type IProcessorExecutor interface {
-	// An Executor expects a processor to be able to handle the results it produce.
-	Result(message.Msg, message.MsgContent)
-
-	// Error sends the errors produced during execution to the processor
-	Error(uint8, error)
-
-	// Done tells the processor that all the execution is done and it should be stopped
-	Done()
-}
-
-// IProcessor defines the interface for a processor as a whole
-type IProcessor interface {
-	IProcessorExecutor
-
-	// process runs a Executor on the incoming message
-	process(message.Msg) bool
-
-	// Returns the message routes for the incoming message for processor
-	incomingRoutes() msgRoutes
-
-	lock(msgRoutes)
-
-	// AddSendRoute adds a send route to a stage from the processor
-	AddSendRoute(*stage, sendRoute)
-
-	// Checks if a processor is closed
-	isClosed() bool
-}
 
 // A Processor represents an entity that wraps up a executor and handles
 // things like providing messages to the executor for execution, returning the
 // result appropriately, and sending the result to the sndPool for further execution.
 type Processor struct {
 	id         uint32             // id of the transforms
-	procPool   *processorPool     //
+	procPool   IProcessorPool     //
 	executor   Executor           // executor associated with the Processor, can have only one executor
-	routes     msgRoutes          // routes for which the Processor should Execute the messages
+	routes     msgRoutes          // routes for which the Processor should execute the messages
 	sndPool    sendPool           // The sndPool to fanout the messages produced by this Processor to all its listeners
 	errSender  chan<- message.Msg //
 	mesFactory message.Factory    // Msg Factory associated with the Processor. Helps in generating new messages.
@@ -69,16 +39,12 @@ func (pr *Processor) lock(stgRoutes msgRoutes) {
 	}
 }
 
-func (pr *Processor) AddSendRoute(stg *stage, route sendRoute) {
-	pr.sndPool.sndRoutes[stg] = route
-}
-
-// process executes the corresponding executor of the Execute on the passed
+// process executes the corresponding executor of the execute on the passed
 // msg. Returns true on successful execution of the executor on msg.
 func (pr *Processor) process(msg message.Msg) bool {
 
 	if pr.executor.ExecutorType() == SOURCE {
-		pod := newMsgPod(pr.statusMessage(pr.procPool.stage.withTrace))
+		pod := newMsgPod(pr.statusMessage(pr.procPool.stage().withTrace))
 		msg = pod.msg
 	}
 
@@ -101,7 +67,7 @@ func (pr *Processor) Result(srcMsg message.Msg, content message.MsgContent) {
 	pr.procLock.Lock()
 	// If sndPool can't send the messages, then there's no point in processing, so Done
 	if !pr.sndPool.send(m, false) {
-		println("Closing proc, could not send ", pr.sndPool.proc.procPool.stage.name)
+		println("Closing proc, could not send ", pr.sndPool.proc.procPool.stage().name)
 		pr.Done()
 	}
 	pr.procLock.Unlock()
@@ -146,6 +112,10 @@ func (pr *Processor) isConnected() bool {
 	return pr.sndPool.isConnected()
 }
 
+func (pr *Processor) processorPool() IProcessorPool {
+	return pr.procPool
+}
+
 // statusMessage creates a new msg with certain variables of interest.
 func (pr *Processor) statusMessage(withTrace bool) message.Msg {
 	status := make(message.MsgContent)
@@ -174,7 +144,7 @@ func newProcessorFactory(stage *stage) processorFactory {
 func (factory *processorFactory) new(executor Executor, routeMap msgRoutes) *Processor {
 	p := &Processor{
 		id:        atomic.AddUint32(&factory.hwm, 1),
-		procPool:  &factory.stage.processorPool,
+		procPool:  factory.stage.processorPool,
 		executor:  executor,
 		routes:    routeMap,
 		errSender: factory.stage.pipeline.errorReceiver,
