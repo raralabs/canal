d83bb33cf7d5d1ccc197d5c58478fb3293f2e64c
n-is <nschl.npl@gmail.com>
2020-05-20 09:12:19 +0545

    Add diagram for example in core.cycle

--- docs.sh
+++ docs.sh
@@ -4,37 +4,33 @@
 win=true
 
 hash godoc.exe 2>/dev/null || {
-        win=false
+    win=false
 }
 
-if [ "$win" = true ]
-then
-        godoc=godoc.exe
-        python=python.exe
+if [ "$win" = true ]; then
+    godoc=godoc.exe
+    python=python.exe
 fi
 
 # Check if the godoc is available in linux
-if [ "$win" = false ]
-then
-        linux=true
-        hash godoc 2>/dev/null || {
-                linux=false
-        }
-        if [ "$linux" = true ]
-        then
-                godoc=godoc
-                python=python
-        fi
+if [ "$win" = false ]; then
+    linux=true
+    hash godoc 2>/dev/null || {
+        linux=false
+    }
+    if [ "$linux" = true ]; then
+        godoc=godoc
+        python=python
+    fi
 fi
 
-if [ "$win" = true ] || [ "$linux" = true ]
-then
-        # Run the godoc and run the server
-        export python="$python"
-        bash -c 'sleep 5; "$python" -mwebbrowser http://localhost:8080/pkg/github.com/raralabs/canal/; exit' &
+if [ "$win" = true ] || [ "$linux" = true ]; then
+    # Run the godoc and run the server
+    export python="$python"
+    bash -c 'sleep 5; "$python" -mwebbrowser http://localhost:8080/pkg/github.com/raralabs/canal/; exit' &
 
-        echo "Generating Documentation. Please Wait."
-        "$godoc" -http=:8080
+    echo "Generating Documentation. Please Wait."
+    "$godoc" -http=:8080
 else
-        echo "godoc not found in system path"
+    echo "godoc not found in system path"
 fi
