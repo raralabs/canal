52aee78e375bdb906a4358783b551c0e631d1c40
n-is <nschl.npl@gmail.com>
2020-07-02 12:19:19 +0545

    Update pass plugin.

--- ext/transforms/doPass.go
+++ ext/transforms/doPass.go
@@ -1,28 +1,17 @@
 package transforms
 
 import (
-	"sync/atomic"
-	"time"
-
 	"github.com/raralabs/canal/core/message"
 	"github.com/raralabs/canal/core/pipeline"
-	"github.com/raralabs/canal/ext/transforms/base_transforms"
+	"github.com/raralabs/canal/core/transforms/do"
 )
 
 func PassFunction() pipeline.Executor {
-	var cnt int32 = 0
 
 	df := func(m message.Msg, proc pipeline.IProcessorForExecutor) bool {
-		time.Sleep(250 * time.Millisecond)
 		proc.Result(m, m.Content())
-		atomic.AddInt32(&cnt, 1)
-		if cnt >= 20 {
-			//println("PassFunction Closing processor in ", proc.Id())
-			proc.Done()
-		}
-
 		return true
 	}
 
-	return base_transforms.NewDoOperator(df)
+	return do.NewOperator(df)
 }
