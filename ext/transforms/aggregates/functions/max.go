5358622f6cd0524feccc243bffbcb0edc4c459e7
n-is <nschl.npl@gmail.com>
2020-07-09 20:00:02 +0545

    Add aggregator functions for: avg, count, max, min & variance.

---
+++ ext/transforms/aggregates/functions/max.go
@@ -0,0 +1,76 @@
+package functions
+
+import (
+	"github.com/raralabs/canal/core/message"
+	"github.com/raralabs/canal/core/transforms/agg"
+	"github.com/raralabs/canal/utils/cast"
+)
+
+type Max struct {
+	maxVal *message.MsgFieldValue
+	tmpl agg.IAggFuncTemplate
+}
+
+func NewMax(tmpl agg.IAggFuncTemplate) *Max {
+	return &Max{
+		tmpl: tmpl,
+		maxVal: message.NewFieldValue(nil, message.NONE),
+	}
+}
+
+func (c *Max) Add(content *message.OrderedContent) {
+	if c.tmpl.Filter(content.Values()) {
+		val, ok := content.Get(c.tmpl.Field())
+		if !ok {
+			return
+		}
+
+		if c.maxVal.Value() == nil {
+			c.maxVal.Val = val.Value()
+			c.maxVal.ValType = val.ValueType()
+			return
+		}
+
+		switch val.ValueType() {
+		case message.INT:
+			m, _ := cast.TryInt(val.Value())
+			cmp, _ := cast.TryInt(c.maxVal.Value())
+
+			mx := maxi(m, cmp)
+			c.maxVal.Val = mx
+
+		case message.FLOAT:
+			m, _ := cast.TryFloat(val.Value())
+			cmp, _ := cast.TryFloat(c.maxVal.Value())
+
+			mx := maxf(m, cmp)
+			c.maxVal.Val = mx
+		}
+	}
+}
+
+func (c *Max) Result() *message.MsgFieldValue {
+	return message.NewFieldValue(c.maxVal.Value(), c.maxVal.ValueType())
+}
+
+func (c *Max) Name() string {
+	return c.tmpl.Name()
+}
+
+func (c *Max) Reset() {
+}
+
+
+func maxi(a, b int64) int64 {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func maxf(a, b float64) float64 {
+	if a > b {
+		return a
+	}
+	return b
+}
---
+++ ext/transforms/aggregates/functions/variance.go
@@ -0,0 +1,73 @@
+package functions
+
+import (
+	"github.com/raralabs/canal/core/message"
+	"github.com/raralabs/canal/core/transforms/agg"
+	"github.com/raralabs/canal/utils/cast"
+	"sync/atomic"
+)
+
+type Variance struct {
+	tmpl agg.IAggFuncTemplate
+
+	num      uint64  // The number of elements that has arrived
+	lastMean float64 // Mean of the last data
+	lastV    float64 // The v
+	variance *message.MsgFieldValue
+}
+
+func NewVariance(tmpl agg.IAggFuncTemplate) *Variance {
+	return &Variance{
+		tmpl: tmpl,
+		variance: message.NewFieldValue(nil, message.NONE),
+		num: uint64(0),
+		lastMean: float64(0),
+		lastV: float64(0),
+	}
+}
+
+func (c *Variance) Add(content *message.OrderedContent) {
+	if c.tmpl.Filter(content.Values()) {
+		val, ok := content.Get(c.tmpl.Field())
+		if !ok {
+			return
+		}
+
+		if c.variance.Value() == nil {
+			c.variance.Val = float64(0)
+			c.variance.ValType = message.FLOAT
+
+			atomic.AddUint64(&c.num, 1)
+			c.lastMean, _ = cast.TryFloat(val.Value())
+			return
+		}
+
+		switch val.ValueType() {
+		case message.INT, message.FLOAT:
+			lastV := c.lastV
+			xk, _ := cast.TryFloat(val.Value())
+
+			atomic.AddUint64(&c.num, 1)
+			newMean := c.lastMean + (xk-c.lastMean)/float64(c.num)
+
+			vk := lastV + (xk-c.lastMean)*(xk-newMean)
+			variance := vk / float64(c.num-1)
+
+			c.lastMean = newMean
+			c.lastV = vk
+
+			c.variance.Val = variance
+		}
+	}
+}
+
+func (c *Variance) Result() *message.MsgFieldValue {
+	return message.NewFieldValue(c.variance.Value(), c.variance.ValueType())
+}
+
+func (c *Variance) Name() string {
+	return c.tmpl.Name()
+}
+
+func (c *Variance) Reset() {
+}
