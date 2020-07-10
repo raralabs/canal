0925efc5a855aa6b8cca6d5c53596de4c513992b
n-is <nschl.npl@gmail.com>
2020-07-10 16:47:46 +0545

    Update take function to support both head and tails.

--- ext/transforms/doFn/take.go
+++ ext/transforms/doFn/take.go
@@ -4,11 +4,33 @@ import (
 	"github.com/raralabs/canal/core/message"
 	"github.com/raralabs/canal/core/pipeline"
 	"github.com/raralabs/canal/core/transforms/do"
+	"github.com/raralabs/canal/utils/dstr"
 )
 
-func TakeHead(num uint64, done func(m message.Msg) bool) pipeline.Executor {
+var descMap = map[string]func(map[string]*dstr.RoundRobin, uint64, []string, *message.OrderedContent){
+	"head": func(table map[string]*dstr.RoundRobin, maxRows uint64, cols []string, content *message.OrderedContent) {
+		if maxRows > 0 {
+			insertMessage(table, cols, content)
+		}
+	},
+	"tail": func(table map[string]*dstr.RoundRobin, maxRows uint64, cols []string, content *message.OrderedContent) {
+		insertMessage(table, cols, content)
+	},
+}
+
+func insertMessage(table map[string]*dstr.RoundRobin, cols []string, content *message.OrderedContent) {
+	for _, key := range cols {
+		if val, ok := content.Get(key); ok {
+			table[key].Put(val)
+		} else {
+			table[key].Put(message.NewFieldValue(nil, message.NONE))
+		}
+	}
+}
+
+func Take(desc string, num uint64, done func(m message.Msg) bool) pipeline.Executor {
 	// Simple Table for Storing required messages
-	table := make(map[string][]*message.MsgFieldValue)
+	table := make(map[string]*dstr.RoundRobin)
 	var cols []string
 	first := true
 
@@ -17,11 +39,12 @@ func TakeHead(num uint64, done func(m message.Msg) bool) pipeline.Executor {
 		mContent := m.Content()
 		if done(m) {
 			if len(cols) > 0 {
-				depth := len(table[cols[0]])
-				for i := 0; i < depth; i++ {
+				depth := table[cols[0]].Len()
+				for i := uint64(0); i < depth; i++ {
 					content := message.NewOrderedContent()
 					for k, v := range table {
-						content.Add(k, v[i])
+						val, _ := v.Get(i)
+						content.Add(k, val.(*message.MsgFieldValue))
 					}
 					proc.Result(m, content)
 				}
@@ -36,19 +59,14 @@ func TakeHead(num uint64, done func(m message.Msg) bool) pipeline.Executor {
 			for e := mContent.First(); e != nil; e = e.Next() {
 				k, _ := e.Value.(string)
 				cols = append(cols, k)
+				table[k] = dstr.NewRoundRobin(num)
 			}
 		}
 
+		descMap[desc](table, num, cols, mContent)
 		if num > 0 {
-			for _, key := range cols {
-				if val, ok := mContent.Get(key); ok {
-					table[key] = append(table[key], val)
-				} else {
-					table[key] = append(table[key], message.NewFieldValue(nil, message.NONE))
-				}
-			}
+			num--
 		}
-		num--
 
 		return false
 	})
