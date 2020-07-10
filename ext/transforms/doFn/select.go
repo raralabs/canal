fdcc16e03d4c0ec31a489b6148e6de19720102c1
n-is <nschl.npl@gmail.com>
2020-07-10 15:03:43 +0545

    Add select function.

---
+++ ext/transforms/doFn/select.go
@@ -0,0 +1,26 @@
+package doFn
+
+import (
+	"github.com/raralabs/canal/core/message"
+	"github.com/raralabs/canal/core/pipeline"
+	"github.com/raralabs/canal/core/transforms/do"
+)
+
+func SelectFunction(fields []string) pipeline.Executor {
+	return do.NewOperator(func(m message.Msg, proc pipeline.IProcessorForExecutor) bool {
+
+		mContent := m.Content()
+		content := message.NewOrderedContent()
+		for _, fld := range fields {
+			if v, ok := mContent.Get(fld); ok {
+				content.Add(fld, v)
+			} else {
+				content.Add(fld, message.NewFieldValue(nil, message.NONE))
+			}
+		}
+
+		proc.Result(m, content)
+
+		return false
+	})
+}
