c7fd8ae9c0816e67123125ab40c46eec4a69a112
n-is <nschl.npl@gmail.com>
2020-07-12 17:35:18 +0545

    Add various stats tests.

---
+++ utils/stream-math/correlation_test.go
@@ -0,0 +1,70 @@
+package stream_math
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestStats(t *testing.T) {
+	x := []float64{1, 2, 3, 6, 8, 9, 45, 12}
+	y := []float64{0, -1, 4, 7, 123, 23, 45, 2}
+
+	assert.Equal(t, len(x), len(y))
+
+	numElements := len(x)
+
+	meanX := NewMean()
+	meanY := NewMean()
+	varX := NewVariance()
+	varY := NewVariance()
+	cov := NewCovariance()
+	corr := NewCorrelation()
+
+	for i := 0; i < numElements; i++ {
+		meanX.Add(x[i])
+		meanY.Add(y[i])
+		varX.Add(x[i])
+		varY.Add(y[i])
+		cov.Add(x[i], y[i])
+		corr.Add(x[i], y[i])
+	}
+
+	mX, _ := meanX.Result()
+	mY, _ := meanY.Result()
+	vX, _ := varX.Result()
+	vY, _ := varY.Result()
+	cv, _ := cov.Result()
+	cr, _ := corr.Result()
+
+	//fmt.Printf("Mean X: %v\n", mX)
+	//fmt.Printf("Mean Y: %v\n", mY)
+	//fmt.Printf("Var  X: %v\n", vX)
+	//fmt.Printf("Var  Y: %v\n", vY)
+	//fmt.Printf("Cov   : %v\n", cv)
+	//fmt.Printf("Corr  : %v\n", cr)
+
+	t.Run("Test MeanX", func(t *testing.T) {
+		assert.Equal(t, 10.75, mX)
+	})
+
+	t.Run("Test MeanY", func(t *testing.T) {
+		assert.Equal(t, 25.375, mY)
+	})
+
+	t.Run("Test VarianceX", func(t *testing.T) {
+		assert.Equal(t, 179.93749999999997, vX)
+	})
+
+	t.Run("Test VarianceY", func(t *testing.T) {
+		assert.Equal(t, 1575.234375, vY)
+	})
+
+	t.Run("Test Covariance", func(t *testing.T) {
+		assert.Equal(t, 138.71875, cv)
+	})
+
+	t.Run("Test Correlation", func(t *testing.T) {
+		assert.Equal(t, 0.26055639413667897, cr)
+	})
+}
---
+++ utils/stream-math/pvalue_test.go
@@ -0,0 +1,28 @@
+package stream_math
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPValue(t *testing.T) {
+	x := []float64{1, 2, 3, 6, 8, 9, 45, 12}
+	y := []float64{0, -1, 4, 7, 123, 23, 45, 2}
+
+	assert.Equal(t, len(x), len(y))
+
+	numElements := len(x)
+	pval := NewPValue()
+
+	for i := 0; i < numElements; i++ {
+		pval.Add(x[i], y[i])
+	}
+
+	res, err := pval.Result()
+
+	assert.Nil(t, err)
+	//fmt.Println(res)
+
+	assert.Equal(t, 0.5331177695581131, res)
+}
