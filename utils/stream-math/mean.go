95fcae8af894c497348c27d2a6bea78acfec1a4e
n-is <nschl.npl@gmail.com>
2020-07-14 19:14:34 +0545

    Update avg agg func to support stream agg.

--- utils/stream-math/mean.go
+++ utils/stream-math/mean.go
@@ -1,29 +1,47 @@
 package stream_math
 
-import "sync/atomic"
+import (
+	"github.com/raralabs/canal/utils/dstr"
+	"sync/atomic"
+)
 
 type Mean struct {
-	lastMean float64
-	num      uint64
+	means *dstr.RoundRobin
+	bufSz uint64
+	num   uint64
 }
 
-func NewMean() *Mean {
+func NewMean(bufSz uint64) *Mean {
 	return &Mean{
-		lastMean: 0,
-		num:      0,
+		means: dstr.NewRoundRobin(bufSz),
+		num:   0,
 	}
 }
 
 func (m *Mean) Add(v float64) {
+	mean := float64(0)
+	if m.num > 0 {
+		lst, _ := m.means.GetLast()
+		mean, _ = lst.(float64)
+	}
 	atomic.AddUint64(&m.num, 1)
-	m.lastMean = m.lastMean + (v-m.lastMean)/float64(m.num)
+	mean = mean + (v-mean)/float64(m.num)
+	m.means.Put(mean)
+}
+
+func (m *Mean) Pop() {
+	m.num--
+	m.means.Pop()
 }
 
 func (m *Mean) Result() (float64, error) {
-	return m.lastMean, nil
+	v, _ := m.means.GetLast()
+	val, _ := v.(float64)
+	return val, nil
 }
 
 func (m *Mean) Reset() {
-	m.lastMean = 0
+	m.means.Dispose()
+	m.means = dstr.NewRoundRobin(m.bufSz)
 	m.num = 0
 }
