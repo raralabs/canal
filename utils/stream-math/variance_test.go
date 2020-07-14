d4ab7481512f66860b0548ab4fd66194ad69952f
n-is <nschl.npl@gmail.com>
2020-07-14 22:38:32 +0545

    Update stream math tests.

---
+++ utils/stream-math/variance_test.go
@@ -0,0 +1,24 @@
+package stream_math
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestVariance(t *testing.T) {
+	data := []float64{2, 1, 3, 4, 5}
+
+	variance := NewVariance()
+
+	for _, d := range data {
+		variance.Add(d)
+	}
+
+	res, _ := variance.Result()
+	assert.Equal(t, float64(2), res)
+
+	variance.Replace(1, 3)
+	res, _ = variance.Result()
+	assert.Equal(t, "1.04", fmt.Sprintf("%.2f", res))
+}
