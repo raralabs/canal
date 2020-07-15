063b1d960856ba22b610a5be454d7fd03a3b9ca9
n-is <nschl.npl@gmail.com>
2020-07-15 23:39:37 +0545

    Add aggregator funcs and templates for Mode.

---
+++ ext/transforms/aggregates/templates/mode.go
@@ -0,0 +1,39 @@
+package templates
+
+import (
+	"github.com/raralabs/canal/core/transforms/agg"
+	"github.com/raralabs/canal/ext/transforms/aggregates/functions"
+)
+
+type Mode struct {
+	name   string
+	filter func(map[string]interface{}) bool
+	field  string
+}
+
+func NewMode(alias, field string, filter func(map[string]interface{}) bool) *Avg {
+	if alias == "" {
+		alias = "Mode"
+	}
+	return &Avg{
+		name:   alias,
+		filter: filter,
+		field:  field,
+	}
+}
+
+func (c *Mode) Filter(m map[string]interface{}) bool {
+	return c.filter(m)
+}
+
+func (c *Mode) Function() agg.IAggFunc {
+	return functions.NewMode(c)
+}
+
+func (c *Mode) Name() string {
+	return c.name
+}
+
+func (c *Mode) Field() string {
+	return c.field
+}
