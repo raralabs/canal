a18c47d8320990efb74a2722ffdca08aaeb98f9b
n-is <nschl.npl@gmail.com>
2020-07-15 23:16:31 +0545

    Update distinct count agg func.

--- ext/transforms/aggregates/templates/dcount.go
+++ ext/transforms/aggregates/templates/dcount.go
@@ -5,37 +5,35 @@ import (
 	"github.com/raralabs/canal/ext/transforms/aggregates/functions"
 )
 
-// HLLpp uses HyperLogLog++ to count distinct number
-// of elements
-type HLLpp struct {
+type DCount struct {
 	name   string
 	filter func(map[string]interface{}) bool
 	field  string
 }
 
-func NewHLLpp(alias, field string, filter func(map[string]interface{}) bool) *HLLpp {
+func NewHLLpp(alias, field string, filter func(map[string]interface{}) bool) *DCount {
 	if alias == "" {
 		alias = "Distinct_Count"
 	}
-	return &HLLpp{
+	return &DCount{
 		name:   alias,
 		filter: filter,
 		field:  field,
 	}
 }
 
-func (H *HLLpp) Name() string {
+func (H *DCount) Name() string {
 	return H.name
 }
 
-func (H *HLLpp) Field() string {
+func (H *DCount) Field() string {
 	return H.field
 }
 
-func (H *HLLpp) Filter(m map[string]interface{}) bool {
+func (H *DCount) Filter(m map[string]interface{}) bool {
 	return H.filter(m)
 }
 
-func (H *HLLpp) Function() agg.IAggFunc {
-	return functions.NewHLLpp(H)
+func (H *DCount) Function() agg.IAggFunc {
+	return functions.NewDCount(H)
 }
