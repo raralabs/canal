a2a2679fdae987c09c2c3e0e7895afe8dbb1052b
n-is <nschl.npl@gmail.com>
2020-07-28 11:10:12 +0545

    Update filter to support message removal from subsequent stages.

--- ext/transforms/doFn/filter.go
+++ ext/transforms/doFn/filter.go
@@ -6,20 +6,32 @@ import (
 	"github.com/raralabs/canal/core/transforms/do"
 )
 
-func FilterFunction(filter func(m message.Msg) (bool, bool, error)) pipeline.Executor {
+func FilterFunction(filter func(map[string]interface{}) (bool, error), doneFunc func(msg message.Msg) bool) pipeline.Executor {
 	return do.NewOperator(func(m message.Msg, proc pipeline.IProcessorForExecutor) bool {
 
-		match, done, err := filter(m)
+		sent := false
+		if content := m.Content(); content != nil {
+			match, err := filter(content.Values())
 
-		if err == nil {
-			if done {
-				proc.Result(m, m.Content(), m.PrevContent())
-				proc.Done()
-				return false
+			if err == nil {
+				if doneFunc(m) {
+					proc.Result(m, m.Content(), m.PrevContent())
+					proc.Done()
+					return false
+				}
+
+				if match {
+					sent = true
+					proc.Result(m, m.Content(), m.PrevContent())
+				}
 			}
+		}
 
-			if match {
-				proc.Result(m, m.Content(), m.PrevContent())
+		if !sent {
+			if pContent := m.PrevContent(); pContent != nil {
+				if ok, _ := filter(pContent.Values()); ok {
+					proc.Result(m, nil, m.PrevContent())
+				}
 			}
 		}
 
