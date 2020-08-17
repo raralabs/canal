c34a60d974ce8e89aabc8430d675ff1fa994aaca
n-is <nschl.npl@gmail.com>
2020-08-17 11:13:31 +0545

    Create simple benchmarks and profile cpu and memory usages.

---
+++ examples/benchs/simple_test.go
@@ -0,0 +1,47 @@
+package benchs
+
+import (
+	"context"
+	"io/ioutil"
+	"log"
+	"runtime"
+	"testing"
+	"time"
+
+	"github.com/raralabs/canal/core/pipeline"
+	"github.com/raralabs/canal/ext/sinks"
+	"github.com/raralabs/canal/ext/sources"
+)
+
+func benchSimple(b *testing.B, n int64) {
+
+	log.SetFlags(0)
+	log.SetOutput(ioutil.Discard)
+
+	p := pipeline.NewPipeline(1)
+
+	src := p.AddSource("Fake Source")
+	sp := src.AddProcessor(pipeline.DefaultProcessorOptions, sources.NewFaker(n, nil))
+
+	sink := p.AddSink("Blackhole Sink")
+	sink.AddProcessor(pipeline.DefaultProcessorOptions, sinks.NewBlackholeSink(), "sink")
+
+	sink.ReceiveFrom("sink", sp)
+
+	c, cancel := context.WithTimeout(context.Background(), 1000*time.Second)
+	p.Validate()
+
+	// Reset Timer
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		p.Start(c, cancel)
+		runtime.GC()
+		runtime.GC()
+		runtime.GC()
+		runtime.GC()
+	}
+
+	cancel()
+}
+
+func BenchmarkSimple_1M(b *testing.B) { benchSimple(b, 1000000) }
