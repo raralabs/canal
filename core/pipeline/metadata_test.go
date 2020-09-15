ccd4a31c52eb73264c642f994d1ca4af4a1cda0e
subodh7591 <subshakya591@gmail.com>
2020-09-15 16:19:48 +0545

    issue in message resolved, added test cases for metadata

--- core/pipeline/metadata_test.go
+++ core/pipeline/metadata_test.go
@@ -11,12 +11,17 @@ import (
 	"time"
 )
 
-//func TestMetaData_newMetaData(t *testing.T){
-//	t.Run("newMetaData", func(t *testing.T) {
-//		metaData := newMetadata()
-//	})
-//
-//}
+func TestMetaData_newMetaData(t *testing.T){
+	t.Run("newMetaData", func(t *testing.T) {
+		metaData := newMetadata()
+		assert.Equal(t,"*sync.Mutex",reflect.TypeOf(metaData.dataMu).String())
+		assert.Equal(t,uint64(30),metaData.msgPerSec.bufSize,"Buffer size doesn't match")
+		assert.Equal(t,uint64(15),metaData.msgPerMin.bufSize,"Buffer size doesn't match")
+		assert.Equal(t,uint64(10),metaData.msgPerHr.bufSize,"Buffer size doesn't match")
+		assert.Equal(t,uint64(7),metaData.msgPerDay.bufSize,"Buffer size doesn't match")
+	})
+
+}
 
 func TestMetaData_messageRate(t *testing.T){
 	t.Run("newMetaData", func(t *testing.T) {
@@ -35,7 +40,6 @@ func TestMetaData_MessageRate_Attr(t *testing.T){
 	metaData := newMetadata()
 	t.Run("start Message Rate", func(t *testing.T) {
 		assert.Equal(t,false,metaData.msgPerSec.tmr.TimerRunning(),"Timer not started")
-		metaData.msgPerSec.tmr.TimerRunning()
 		metaData.msgPerSec.start()
 		assert.Equal(t,true,metaData.msgPerSec.tmr.TimerRunning(),"Timer started")
 		buf := metaData.msgPerSec.data()
@@ -66,13 +70,21 @@ func TestMetaData_init(t *testing.T){
 func TestMetaData_ping(t *testing.T){
 	msg:= message.Msg{}
 	metaData := newMetadata()
-	t.Run("lock", func(t *testing.T) {
+	t.Run("Ping Meta Data", func(t *testing.T) {
 		assert.Equal(t,uint64(0),metaData.totalRcvMsg)
-		metaData.ping(msg)
-		assert.Equal(t,uint64(1),metaData.totalRcvMsg)
+		for i:=1;i<5;i++{
+			metaData.ping(msg)
+			assert.Equal(t,uint64(i),metaData.totalRcvMsg,"total received message should be %d",i)
+			assert.Equal(t,msg.Id(),metaData.lastRcvMid,"message id didn't match")
+			assert.Equal(t,uint64(i),metaData.msgPerSec.counter,"unexpect value of the counter")
+			assert.Equal(t,uint64(i),metaData.msgPerMin.counter,"unexpect value of the counter")
+			assert.Equal(t,uint64(i),metaData.msgPerHr.counter,"unexpect value of the counter")
+			assert.Equal(t,uint64(i),metaData.msgPerDay.counter,"unexpect value of the counter")
+		}
+
+
 	})
 }
-
 func TestMetaData_lock(t *testing.T){
 	metaData := newMetadata()
 	t.Run("lock", func(t *testing.T) {
