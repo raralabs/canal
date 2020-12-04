99bf8445b82698ab5e1665d318590c0c0b093892
Subodh Shakya <subshakya591@gmail.com>
2020-12-04 16:07:42 +0545

    minor changes on liftBridgeWriter.go

--- ext/sinks/liftBridgeWriter.go
+++ ext/sinks/liftBridgeWriter.go
@@ -10,7 +10,7 @@ import (
 //struct to hold the required attributes of the lift bridge writer
 type LiftBridgeWriter struct {
 	name       string              //name of the writer
-	ports 	   []string				//ports to publish the msg
+	addrs 	   []string				//ports to publish the msg
 	streamName string				//name of the stream of nats
 	subject    string				//name of the subject of the nats from which the msg is published
 
@@ -19,41 +19,40 @@ type LiftBridgeWriter struct {
 
 //initializes the liftbridge writer as per the given parameters
 func NewLiftBridgeWriter(streamName,subject string ,ports ...int64) pipeline.Executor {
-	var connectPorts  []string
+	var addrs []string
 	for _,port := range ports{
-		connectPorts = append(connectPorts,strconv.FormatInt(port, 10))
+		prefixAddrs := "localhost:"
+		fullAddr := prefixAddrs + strconv.FormatInt(port,10)
+		addrs = append(addrs,fullAddr)
+
 	}
 
-	return &LiftBridgeWriter{name: "liftBridgeWriter",ports :connectPorts,subject: subject,streamName: streamName}
+	return &LiftBridgeWriter{name: "liftBridgeWriter",addrs :addrs,subject: subject,streamName: streamName}
 }
 
 func (lyft *LiftBridgeWriter) ExecutorType() pipeline.ExecutorType {
 	return pipeline.SINK
 }
 
-//old implementation of the code
-//func (s *BlackholeSink) Execute(m message.Msg, pr pipeline.IProcessorForExecutor) bool {
-//	return true
-//}
 
 func (lyft *LiftBridgeWriter) Execute(m pipeline.MsgPod, _ pipeline.IProcessorForExecutor) bool {
-	addrs := []string{}
+	//array of string to hold the ip and port
+	//to which the connection is to be made
+
 	ctx:= context.Background()
-	for _,port := range lyft.ports{
-		prefixAddrs := "localhost:"
-		fullAddr := prefixAddrs + port
-		addrs = append(addrs,fullAddr)
-	}
-	client, err := lift.Connect(addrs)
+	//make a connection to the address
+	client, err := lift.Connect(lyft.addrs)
 	if err != nil {
 		panic(err)
 	}
 	defer client.Close()
+	//create a stream with given subject and name
 	if err := client.CreateStream(ctx,lyft.subject, lyft.name); err != nil {
 		if err != lift.ErrStreamExists {
 			panic(err)
 		}
 	}
+	//write the published msg to the log
 	if _, err := client.Publish(ctx, lyft.streamName, []byte(m.Msg.String())); err != nil {
 		panic(err)
 	}
